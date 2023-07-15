5c05a7db482e03c2f696954fa434475dd077759f
Brenosalv <brenosalvadordefreitas@gmail.com>
2023-07-15 17:22:21 -0300

    Add post content to post page

--- src/pages/Post/index.tsx
+++ src/pages/Post/index.tsx
@@ -1,10 +1,17 @@
+import { PostContent } from "../../modules/PostContent";
 import { PostSummary } from "../../modules/PostSummary";
 import { Container } from "./styles";
 
 export function Post() {
   return (
     <Container>
-      <PostSummary />
+      <header>
+        <PostSummary />
+      </header>
+
+      <main>
+        <PostContent />
+      </main>
     </Container>
   )
 }
